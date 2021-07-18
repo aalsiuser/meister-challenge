9a224fda1a1a8070be85b4fe6ec34caf4eabe6f6
aalsiuser <vamshee3000@gmail.com>
2021-07-18 18:04:46 +0530

    Added validation for presence and enum status for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates_presence_of :title, :content, :summary
+
   belongs_to :user, foreign_key: :author_id
   has_many :comments, dependent: :destroy
+
+  enum status: %i[draft inprogress completed published]
 end
