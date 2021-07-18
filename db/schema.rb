948dbdb956cc74b74c1fdf161988aa989d854157
aalsiuser <vamshee3000@gmail.com>
2021-07-18 18:05:33 +0530

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_18_105230) do
+ActiveRecord::Schema.define(version: 2021_07_18_115225) do
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_07_18_105230) do
     t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
