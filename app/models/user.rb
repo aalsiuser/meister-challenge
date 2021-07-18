27da6a11af941d864f9cb07986aaa9cfa7d5b8d5
aalsiuser <vamshee3000@gmail.com>
2021-07-18 20:58:31 +0530

    Added unique validation for email for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :first_name, :email
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates_uniqueness_of :email
 
   has_many :posts, foreign_key: :author_id, dependent: :destroy
 end
