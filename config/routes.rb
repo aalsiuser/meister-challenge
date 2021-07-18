0c8d12a29d1fedf3a2f1ea4cf0a1d1eee13d276a
aalsiuser <vamshee3000@gmail.com>
2021-07-18 22:04:58 +0530

    Added nested route for comment_reaction

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :posts do
+      resources :posts, shallow: true do
         # https://guides.rubyonrails.org/routing.html#shallow-nesting
-        resources :comments, shallow: true
+        resources :comments do
+          resources :reactions
+        end
       end
     end
   end
