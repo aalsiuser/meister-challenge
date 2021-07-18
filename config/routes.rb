d112d7ce9d6192978420b73d765d384ef9a40b1f
aalsiuser <vamshee3000@gmail.com>
2021-07-18 19:26:07 +0530

    Added routes with shallow nesting

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :posts
+      resources :posts do
+        # https://guides.rubyonrails.org/routing.html#shallow-nesting
+        resources :comments, shallow: true
+      end
     end
   end
 end
