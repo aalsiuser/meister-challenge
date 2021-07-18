aa331d5b9c7494472569fb7cb61c87d0d5142f6f
aalsiuser <vamshee3000@gmail.com>
2021-07-18 20:59:08 +0530

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+              [
+                { first_name: 'Monica', last_name: 'Geller', email: 'monica@gmail.com' },
+                { first_name: 'Joe', last_name: '', email: 'joe@gmail.com' },
+                { first_name: 'Ross', last_name: 'Geller', email: 'ross@gmail.com' },
+                { first_name: 'Chandler', last_name: 'Bing', email: 'chandler@gmail.com' },
+                { first_name: 'Rachel', last_name: 'Green', email: 'rachel@gmail.com' }
+              ]
+            )
+
+Post.create(
+            [
+              { title: 'Blog Title 1', content: 'Content 1', summary: 'Summary 1', author_id: 1, status: 3 },
+              { title: 'Blog Title 2', content: 'Content 2', summary: 'Summary 2', author_id: 2, status: 2 },
+              { title: 'Blog Title 3', content: 'Content 3', summary: 'Summary 3', author_id: 2, status: 3 },
+              { title: 'Blog Title 4', content: 'Content 4', summary: 'Summary 4', author_id: 3, status: 2 },
+              { title: 'Blog Title 5', content: 'Content 5', summary: 'Summary 5', author_id: 3, status: 3 }
+            ]
+           )
+Comment.create(
+                [
+                  { user_id: 4, post_id: 1, content: 'Comment 1' },
+                  { user_id: 4, post_id: 1, content: 'Comment 2' },
+                  { user_id: 4, post_id: 1, content: 'Comment 3' },
+                  { user_id: 5, post_id: 2, content: 'Comment 4' },
+                  { user_id: 5, post_id: 3, content: 'Comment 5' },
+                  { user_id: 5, post_id: 3, content: 'Comment 6' },
+                  { user_id: 5, post_id: 2, content: 'Comment 7' },
+                  { user_id: 2, post_id: 4, content: 'Comment 8' },
+                  { user_id: 2, post_id: 4, content: 'Comment 9' },
+                  { user_id: 1, post_id: 5, content: 'Comment 10' },
+                  { user_id: 1, post_id: 5, content: 'Comment 11' }
+                ]
+              )
