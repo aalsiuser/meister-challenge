1c5eaa4c32f0fa086aa7dfb3f460f3e7b58ca12c
aalsiuser <vamshee3000@gmail.com>
2021-07-18 18:05:07 +0530

    Added validation presence for user and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :first_name, :email
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   has_many :posts, foreign_key: :author_id, dependent: :destroy
 end
