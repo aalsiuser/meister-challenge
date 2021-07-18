3859d8c9c2d8810516cc135c92752223da900a03
aalsiuser <vamshee3000@gmail.com>
2021-07-18 17:00:30 +0530

    Added central error module to whole api_controller wrapper

---
+++ lib/error/api_error_handler.rb
@@ -0,0 +1,45 @@
+require 'error/helpers/render.rb'
+module Error
+  module ApiErrorHandler
+    extend ActiveSupport::Concern
+
+    included do
+      # Handle ActiveRecord NotFound error
+      rescue_from ActiveRecord::RecordNotFound do |e|
+        respond(:not_found, 404, 'Record that you are looking for does not exist.')
+      end
+
+      rescue_from ActionController::ParameterMissing do |e|
+        respond(:parameter_missing, 400, e.to_s)
+      end
+
+      rescue_from ActiveRecord::RecordInvalid do |e|
+        respond(:unprocessable_entity, 422, e.record.errors.full_messages.join(', '))
+      end
+
+      rescue_from ActiveRecord::RecordNotUnique do |e|
+        respond(:data_error, 422, e.to_s)
+      end
+
+      rescue_from ActiveRecord::StatementInvalid do |e|
+        respond(:data_error, 422, e.to_s)
+      end
+
+      rescue_from ForbiddenError do |e|
+        respond(:forbidden, 403, e.to_s)
+      end
+
+      rescue_from ArgumentError do |e|
+        respond(:argument_error, 422, e.to_s)
+      end
+    end
+
+    private
+
+    def respond(error, status, message)
+      json = Helpers::Render.json(error, status, message)
+      render json: json, status: status
+    end
+
+  end
+end
