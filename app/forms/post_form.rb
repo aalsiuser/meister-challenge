81fac03f02aaf498944c5b0ab825f9793e897b22
aalsiuser <vamshee3000@gmail.com>
2021-07-18 18:04:25 +0530

    Added form object for post

---
+++ app/forms/post_form.rb
@@ -0,0 +1,36 @@
+class PostForm
+  extend Dry::Initializer[undefined: false]
+
+  # Mandatory Attributes
+  option :title
+  option :content
+  option :summary
+  option :user_id
+
+  # Optional Attributes
+  option :id, optional: true
+
+  attr_reader :record
+
+  def save
+    @record = build_post
+     # Assign attributes for post
+     assign_attributes
+     @record.save!
+  end
+
+  private
+
+  def build_post
+    id.present? Post.find(id) : Post.new
+  end
+
+  def save
+    @record.assing_attributes(
+      title: title,
+      content: content,
+      summary: summary,
+      author_id: user_id
+    )
+  end
+end
