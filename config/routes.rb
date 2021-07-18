8806a67040d3f23de42d736aa5e61bd369319d2e
aalsiuser <vamshee3000@gmail.com>
2021-07-19 01:17:23 +0530

    Added shallow routes to avoid nestd routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :posts, shallow: true do
-        # https://guides.rubyonrails.org/routing.html#shallow-nesting
-        resources :comments do
-          resources :reactions
+      # https://guides.rubyonrails.org/routing.html#shallow-nesting
+      resources :posts, only: %i[index show create update destroy], shallow: true do
+        resources :comments, only: %i[index show create update destroy] do
+          resources :reactions, only: %i[create destroy]
         end
       end
     end
